b535c262b800e631ae6bad30b636c2d933f8df1a
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-06-30 20:48:31 +0700

    feat: bump up context version to use react-router and react v19

--- client-using-context/src/main.jsx
+++ client-using-context/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router";
 
 import { SocketProvider } from "./contexts/socket-context.jsx";
 import router from "./routers/index.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<SocketProvider>
-			<RouterProvider router={router} />
-		</SocketProvider>
-	</StrictMode>,
+  <StrictMode>
+    <SocketProvider>
+      <RouterProvider router={router} />
+    </SocketProvider>
+  </StrictMode>,
 );
--- client-using-context/src/routers/index.jsx
+++ client-using-context/src/routers/index.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
 import MainPage from "../views/MainPage";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <MainPage />,
-	},
+  {
+    path: "/",
+    element: <MainPage />,
+  },
 ]);
 
 export default router;
